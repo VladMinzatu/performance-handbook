4a485f6a34c9247df4419f3969d4343aec1a8dac
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-08-24 11:38:59 +0200

    simple query

--- db-query/go.mod
+++ db-query/go.mod
@@ -1,3 +1,5 @@
 module github.com/VladMinzatu/performance-handbook/db-query
 
 go 1.24.4
+
+require github.com/mattn/go-sqlite3 v1.14.32
--- db-query/main.go
+++ db-query/main.go
@@ -1,4 +1,49 @@
 package main
 
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, name, email FROM users")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			users = append(users, u)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	})
+
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
