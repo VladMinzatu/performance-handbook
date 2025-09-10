8c91d704e22228f301f0752ab459f6745dc41c7d
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-09-10 08:33:31 +0300

    add echo server

---
+++ reverse-proxy/cmd/echo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+	log.Println("Echo server listening on port 9000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	io.Copy(conn, conn)
+}
