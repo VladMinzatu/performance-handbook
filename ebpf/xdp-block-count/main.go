4332be6b69b834c3c7a8f345e5c5ae25a00669e4
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-10-13 16:57:56 +0000

    print top counts periodically

--- ebpf/xdp-block-count/main.go
+++ ebpf/xdp-block-count/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"sort"
+	"syscall"
+	"time"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 )
 
@@ -18,6 +24,8 @@ func main() {
 	defer objs.Close()
 
 	prog := objs.xdp_block_countPrograms.XdpBlockCountProg
+	ipCounters := objs.xdp_block_countMaps.IpCounters
+	// blocklist := objs.xdp_block_countMaps.Blocklist
 
 	ifname := "enp0s1"
 	iface, err := net.InterfaceByName(ifname)
@@ -37,4 +45,56 @@ func main() {
 	defer l.Close()
 
 	fmt.Printf("Attached XDP program to %s (ifindex %d)\n", ifname, iface.Index)
+
+	// Periodically iterate over ip_counters and print top N
+	ticker := time.NewTicker(time.Duration(2) * time.Second)
+	defer ticker.Stop()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	fmt.Println("Press Ctrl-C to detach and exit")
+
+	for {
+		select {
+		case <-ticker.C:
+			printTop(ipCounters, 10)
+		case <-sig:
+			fmt.Println("exiting, detaching XDP...")
+			return
+		}
+	}
+}
+
+func printTop(m *ebpf.Map, topN int) {
+	// iterate map
+	iter := m.Iterate()
+	var key uint32
+	var val uint64
+	type kv struct {
+		ip  uint32
+		cnt uint64
+	}
+	var arr []kv
+	for iter.Next(&key, &val) {
+		arr = append(arr, kv{ip: key, cnt: val})
+	}
+	if err := iter.Err(); err != nil {
+		fmt.Printf("map iterate error: %v\n", err)
+		return
+	}
+	sort.Slice(arr, func(i, j int) bool { return arr[i].cnt > arr[j].cnt })
+	fmt.Printf("Top %d sources (time=%s):\n", topN, time.Now().Format(time.RFC3339))
+	for i := 0; i < len(arr) && i < topN; i++ {
+		fmt.Printf("%2d) %s: %d\n", i+1, uint32ToIP(arr[i].ip).String(), arr[i].cnt)
+	}
+	if len(arr) == 0 {
+		fmt.Println("  (no counters yet)")
+	}
+}
+
+func uint32ToIP(u uint32) net.IP {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, u)
+	return net.IP(b)
 }
