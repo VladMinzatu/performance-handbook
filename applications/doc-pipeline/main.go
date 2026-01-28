befe915c54deda0706b965c3d5ed326492da628b
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-01-28 19:18:24 +0100

    small e2e test

--- applications/doc-pipeline/main.go
+++ applications/doc-pipeline/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
-	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/embed"
-	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/index"
 	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/ingest"
 	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/load"
 	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/pipeline"
-	"github.com/VladMinzatu/performance-handbook/doc-pipeline/internal/tokenize"
 )
 
 func main() {
@@ -36,36 +34,40 @@ func main() {
 	}
 	dataLoadingStage.Run(context.Background(), &sync.WaitGroup{})
 
-	tokenizedDocChan := make(chan tokenize.TokenizedDoc)
-	tokenizeStage := pipeline.Stage[ingest.Document, tokenize.TokenizedDoc]{
-		Name:    "tokenize",
-		Workers: 10,
-		In:      documentChan,
-		Out:     tokenizedDocChan,
-		Fn:      tokenize.Tokenize,
+	for doc := range documentChan {
+		fmt.Println(doc.ID)
 	}
-	tokenizeStage.Run(context.Background(), &sync.WaitGroup{})
 
-	embeddedDocChan := make(chan embed.EmbeddedDoc)
-	embedder := embed.NewEmbedder(1024)
-	embedStage := pipeline.Stage[tokenize.TokenizedDoc, embed.EmbeddedDoc]{
-		Name:    "embed",
-		Workers: 10,
-		In:      tokenizedDocChan,
-		Out:     embeddedDocChan,
-		Fn:      embedder.Embed,
-	}
-	embedStage.Run(context.Background(), &sync.WaitGroup{})
+	// tokenizedDocChan := make(chan tokenize.TokenizedDoc)
+	// tokenizeStage := pipeline.Stage[ingest.Document, tokenize.TokenizedDoc]{
+	// 	Name:    "tokenize",
+	// 	Workers: 10,
+	// 	In:      documentChan,
+	// 	Out:     tokenizedDocChan,
+	// 	Fn:      tokenize.Tokenize,
+	// }
+	// tokenizeStage.Run(context.Background(), &sync.WaitGroup{})
 
-	indexChan := make(chan index.DedupResult)
-	idx := index.NewEmbeddingIndex(0.8)
-	indexStage := pipeline.Stage[embed.EmbeddedDoc, index.DedupResult]{
-		Name:    "index",
-		Workers: 10,
-		In:      embeddedDocChan,
-		Out:     indexChan,
-		Fn:      idx.DedupAndIndex,
-	}
-	indexStage.Run(context.Background(), &sync.WaitGroup{})
+	// embeddedDocChan := make(chan embed.EmbeddedDoc)
+	// embedder := embed.NewEmbedder(1024)
+	// embedStage := pipeline.Stage[tokenize.TokenizedDoc, embed.EmbeddedDoc]{
+	// 	Name:    "embed",
+	// 	Workers: 10,
+	// 	In:      tokenizedDocChan,
+	// 	Out:     embeddedDocChan,
+	// 	Fn:      embedder.Embed,
+	// }
+	// embedStage.Run(context.Background(), &sync.WaitGroup{})
+
+	// indexChan := make(chan index.DedupResult)
+	// idx := index.NewEmbeddingIndex(0.8)
+	// indexStage := pipeline.Stage[embed.EmbeddedDoc, index.DedupResult]{
+	// 	Name:    "index",
+	// 	Workers: 10,
+	// 	In:      embeddedDocChan,
+	// 	Out:     indexChan,
+	// 	Fn:      idx.DedupAndIndex,
+	// }
+	// indexStage.Run(context.Background(), &sync.WaitGroup{})
 
 }
